5b38c723cc5117350e54f2f944d4608dbbef4307
wrriterr <124503930+wrriterr@users.noreply.github.com>
2023-07-23 17:47:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,57 +2,70 @@
 
 let canvas;
 let ctx;
-canvas = document.createElement("canvas")
+canvas = document.getElementById("canvas")
 ctx = canvas.getContext("2d")
-canvas.width = 900;
+canvas.width = 890;
 canvas.height = 550;
 document.body.appendChild(canvas);
 
-let background,hero,beam,ghost,gameover
+
+let bg,hero,lightning,ghost,gameover
 let gameOver = false
+// let restartButton = document.getElementById("restart")
 let score = 0
-//히어로 좌표
 let heroX = canvas.width/2-43;
 let heroY = canvas.height-87;
 
-let beamlist = [] //빔들을 저장하는 리스트
-function beams() {
-    this.x = 0;
-    this.y = 0;
-    this.init = function() {
-        this.x = heroX + 12
-        this.y = heroY
-        this.alive = true // true면 살아있는 빔
-        beamlist.push(this)
-    } //초기화
-this.update  = function() {
-    this.y -= 7;
+let mouse = {
+    x: null, y:null
 }
 
-this.checkHit = function() {
-        
+canvas.addEventListener('click', function(event) {
+    location.reload();
+    mouse.x = event.x;
+    mouse.y = event.y;
+})
+
+
+let lightningList = []
+function lightnings() {
+    this.x = 0
+    this.y = 0
+    this.init = function() {
+        this.x = heroX + 20
+        this.y = heroY
+        this.alive = true
+        lightningList.push(this)
+    }
+    this.update = function() {
+        this.y -= 7;   //번개 속도
+    }
+
+    this.hit = function() {
+
     for(let i = 0; i < ghostList.length; i++) {
-    if(
-        this.y <= ghostList[i].y &&
-        this.x >= ghostList[i].x &&
-        this.x <= ghostList[i].x + 90)
+        if(
+            this.y <= ghostList[i].y &&
+            this.x >= ghostList[i].x &&
+            this.x <= ghostList[i].x + 50)
         {score++;
-        this.alive = false; // 죽은 빔
-        ghostList.splice(i, 1);
+        this.alive = false;
+        ghostList.splice(i, 1)   //맞았으면 빔도 사라지기
         }
     }
   }
 }
 
-function random(min,max) {
-    let randomNum = Math.floor(Math.random()*(max-min+1)) + min
+
+function random(max, min) {
+    let randomNum = Math.floor(Math.random()*(max-min+1))+min
     return randomNum
 }
 
 let ghostList = []
 function ghosts() {
-    this.x = 0;
-    this.y = 0;
+    this.x = 0
+    this.y = 0
     this.init = function() {
         this.y = 0
         this.x = random(0, canvas.width-87)
@@ -62,96 +75,105 @@ function ghosts() {
     this.update = function() {
         this.y += 3;
 
-        if(this.y >= canvas.height - 48) {
+        if(this.y >= canvas.height - 61) {
             gameOver = true;
+            // restartButton.style.display = "flex"
         }
     }
 }
 
+
+// restartButton.addEventListener("click", () => {
+//     location.reload()
+//     })
+
+
 function loadImage() {
-    backgroundImage = new Image();
-    backgroundImage.src = "img/background.PNG";
+    bgImg = new Image();
+    bgImg.src = "img/background.PNG"
 
-    heroImage = new Image();
-    heroImage.src = "img/hero.png";
+    heroImg = new Image();
+    heroImg.src = "img/hero.png";
 
-    beamImage = new Image();
-    beamImage.src = "img/beam.png";
+    lightningImg = new Image();
+    lightningImg.src = "img/lightning.png"
 
-    ghostImage = new Image();
-    ghostImage.src = "img/ghost.png";
+    ghostImg = new Image();
+    ghostImg.src = "img/ghost.png"
 
-    gameoverImage = new Image();
-    gameoverImage.src = "img/gameover.png";
+    gameoverImg = new Image();
+    gameoverImg.src = "img/gameover.png"
+
+    restartImg = new Image();
+    restartImg.src = "img/restart.png"
 }
 
-let keysDown={}
-function setupKeyboardListener() {
+
+let keysDown = {}
+function setupKeyboard() {
     document.addEventListener("keydown", function(event) {
         keysDown[event.keyCode] = true;
     });
     document.addEventListener("keyup", function(event) {
         delete keysDown[event.keyCode];
-    if(event.keyCode == 32){
-        createBeam() // 빔 생성
-    }
+        if(event.keyCode == 32){
+            createLightning()
+        }
     });
 }
 
-function createBeam() {
-    let b = new beams() //빔 하나 생성
+function createLightning() {
+    let b = new lightnings()
     b.init()
 }
 
-
 function createGhost() {
-    const interval  = setInterval(function() {
+    const interval = setInterval(function() {
         let e = new ghosts()
         e.init()
     },1200)
 }
 
+
+
 function update() {
-    if(39 in keysDown) {
-        heroX += 10;     //hero 속도
-    } //right
-    if(37 in keysDown) {
+    if (39 in keysDown) {
+        heroX += 10;
+    }
+    if (37 in keysDown) {
         heroX -= 10;
-    } //left
-    if(heroX <= 0) {
+    }
+    if (heroX <= 0) {
         heroX = 0
     }
     if(heroX > canvas.width-87) {
         heroX = canvas.width-87
     }
-
-    for(let i = 0; i < beamlist.length; i++) {
-        if(beamlist[i].alive) {
-        beamlist[i].update();
-        beamlist[i].checkHit();
+    for(let i = 0; i < lightningList.length; i++) {
+        if(lightningList[i].alive) {
+            lightningList[i].update();
+            lightningList[i].hit();
         }
     }
-
     for(let i = 0; i < ghostList.length; i++) {
         ghostList[i].update();
     }
 }
 
 function render() {
-    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
-    ctx.drawImage(heroImage, heroX, heroY);
-    ctx.fillText(`score : ${score}`, 20, 35);
+    ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height)
+    ctx.drawImage(heroImg, heroX, heroY)
+    ctx.fillText(`score : ${score}`, 20, 35)
     ctx.fillStyle = "white";
-    ctx.font = "25px Arial";
-    
-    for(let i = 0; i < beamlist.length; i++) {
-        if(beamlist[i].alive) {
-            ctx.drawImage(beamImage, beamlist[i].x, beamlist[i].y); 
+    ctx.font = "25px Sans-serif";
+
+    for(let i = 0; i < lightningList.length; i++) {
+        if(lightningList[i].alive) {
+            ctx.drawImage(lightningImg, lightningList[i].x, lightningList[i].y)
         }
     }
-
     for(let i = 0; i < ghostList.length; i++) {
-        ctx.drawImage(ghostImage, ghostList[i].x, ghostList[i].y);
+        ctx.drawImage(ghostImg, ghostList[i].x, ghostList[i].y)
     }
 }
 
@@ -160,12 +182,15 @@ function main() {
     update();
     render();
     requestAnimationFrame(main);
-    } else {
-        ctx.drawImage(gameoverImage, 320, 120, 250, 250);
+    }
+    else {
+        ctx.drawImage(gameoverImg, 320, 20, 250, 250)
+        ctx.drawImage(restartImg, 420, 250, 50, 50)
     }
 }
 
+
 loadImage();
-setupKeyboardListener();
+setupKeyboard();
 main();
 createGhost();
